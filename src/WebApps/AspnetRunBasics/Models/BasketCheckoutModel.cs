1062f88bc387e2b3c703b5ebb8cd7097cd425b4c
Tony Piccirilli <anpicc01@louisville.edu>
2022-01-05 13:57:38 -0500

    Refactor new Web Project

--- src/WebApps/AspnetRunBasics/Models/BasketCheckoutModel.cs
+++ src/WebApps/AspnetRunBasics/Models/BasketCheckoutModel.cs
@@ -1,12 +1,16 @@
-﻿namespace AspnetRunBasics.Entities
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.Models
 {
-    public class Order
+    public class BasketCheckoutModel
     {
-        public int Id { get; set; }
         public string UserName { get; set; }
         public decimal TotalPrice { get; set; }
 
-        // BillingAddress
+        //BillingAddress
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string EmailAddress { get; set; }
@@ -15,18 +19,12 @@
         public string State { get; set; }
         public string ZipCode { get; set; }
 
-        // Payment
+        //Payment
         public string CardName { get; set; }
         public string CardNumber { get; set; }
         public string Expiration { get; set; }
         public string CVV { get; set; }
-        public PaymentMethod PaymentMethod { get; set; }
-    }
+        public int PaymentMethod { get; set; }
 
-    public enum PaymentMethod
-    {
-        CreditCard = 1,
-        DebitCard = 2,
-        Paypal = 3
     }
 }
