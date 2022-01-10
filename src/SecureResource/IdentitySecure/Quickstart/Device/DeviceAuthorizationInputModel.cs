72f726a4b43f239e178089ed72a25aede5111931
Tony Piccirilli <anpicc01@louisville.edu>
2022-01-10 16:03:52 -0500

    Developed Device in Identity Secure Project

---
+++ src/SecureResource/IdentitySecure/Quickstart/Device/DeviceAuthorizationInputModel.cs
@@ -0,0 +1,9 @@
+﻿using IdentitySecure.Quickstart.Consent;
+
+namespace IdentitySecure.Quickstart.Device
+{
+    public class DeviceAuthorizationInputModel : ConsentInputModel
+    {
+        public string UserCode { get; set; }
+    }
+}
---
+++ src/SecureResource/IdentitySecure/Quickstart/Device/DeviceAuthorizationViewModel.cs
@@ -0,0 +1,10 @@
+﻿using IdentitySecure.Quickstart.Consent;
+
+namespace IdentitySecure.Quickstart.Device
+{
+    public class DeviceAuthorizationViewModel : ConsentViewModel
+    {
+        public string UserCode { get; set; }
+        public bool ConfirmUserCode { get; set; }
+    }
+}
